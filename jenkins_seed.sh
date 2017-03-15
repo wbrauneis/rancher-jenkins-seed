3b0da99b05ec2982f86ef16f2c83cae29faaf750
Georgi Potzkov <georgi.potzkov@e-mundo.de>
2017-03-15 09:51:23 +0100

    Mailer Settings added

--- jenkins_seed.sh
+++ jenkins_seed.sh
@@ -21,10 +21,15 @@ while [ ! -f /usr/share/jenkins/rancher-seed/gerrit-trigger.xml ]; do
     sleep 1
 done
 
+while [ ! -f /usr/share/jenkins/rancher-seed/hudson.tasks.Mailer.xml ]; do
+    sleep 1
+done
+
 cp -r /usr/share/jenkins/rancher-seed/.ssh /var/jenkins_home/
 cp -r /usr/share/jenkins/rancher-seed/jobs /var/jenkins_home/
 cp /usr/share/jenkins/rancher-seed/config.xml /var/jenkins_home/config.xml
 cp /usr/share/jenkins/rancher-seed/credentials.xml /var/jenkins_home/credentials.xml
 cp /usr/share/jenkins/rancher-seed/gerrit-trigger.xml /var/jenkins_home/gerrit-trigger.xml
+cp /usr/share/jenkins/rancher-seed/hudson.tasks.Mailer.xml /var/jenkins_home/hudson.tasks.Mailer.xml
 
 exec /bin/tini -- /usr/share/jenkins/rancher/jenkins.sh
