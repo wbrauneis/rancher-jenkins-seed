53a635f8b370ebd9b37a1796d7e7714816cec55c
Georgi Potzkov <georgi.potzkov@e-mundo.de>
2017-03-17 15:41:22 +0100

    fixed copy command of groovy scripts

--- jenkins_seed.sh
+++ jenkins_seed.sh
@@ -10,8 +10,8 @@ while [ ! -f /usr/share/jenkins/rancher-seed/.ssh/id_rsa ]; do
     sleep 1
 done
 
-while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/credentials.groovy ]; do
-    echo 'init.groovy.d/credentials.groovy not found!'
+while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/jenkins-credentials.groovy ]; do
+    echo 'init.groovy.d/jenkins-credentials.groovy not found!'
     sleep 1
 done
 
@@ -20,27 +20,23 @@ while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/emundo-ad.groovy ]; d
     sleep 1
 done
 
-while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/environment-variables.groovy ]; do
-    echo 'init.groovy.d/environment-variables.groovy not found!'
+while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/emundo-environment-variables.groovy ]; do
+    echo 'init.groovy.d/emundo-environment-variables.groovy not found!'
     sleep 1
 done
 
-while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/gerrit-trigger.groovy ]; do
-    echo 'init.groovy.d/gerrit-trigger.groovy not found!'
+while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/emundo-gerrit-trigger.groovy ]; do
+    echo 'init.groovy.d/emundo-gerrit-trigger.groovy not found!'
     sleep 1
 done
 
-while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/mail.groovy ]; do
-    echo 'init.groovy.d/mail.groovy not found!'
+while [ ! -f /usr/share/jenkins/rancher-seed/init.groovy.d/emundo-mail.groovy ]; do
+    echo 'init.groovy.d/emundo-mail.groovy not found!'
     sleep 1
 done
 
 cp -r /usr/share/jenkins/rancher-seed/.ssh /var/jenkins_home/
 cp -r /usr/share/jenkins/rancher-seed/jobs /var/jenkins_home/
-cp /usr/share/jenkins/rancher-seed/init.groovy.d/credentials.groovy /var/jenkins_home/init.groovy.d/credentials.groovy
-cp /usr/share/jenkins/rancher-seed/init.groovy.d/emundo-ad.groovy /var/jenkins_home/init.groovy.d/emundo-ad.groovy
-cp /usr/share/jenkins/rancher-seed/init.groovy.d/environment-variables.groovy /var/jenkins_home/init.groovy.d/environment-variables.groovy
-cp /usr/share/jenkins/rancher-seed/init.groovy.d/gerrit-trigger.groovy /var/jenkins_home/init.groovy.d/gerrit-trigger.groovy
-cp /usr/share/jenkins/rancher-seed/init.groovy.d/mail.groovy /var/jenkins_home/init.groovy.d/mail.groovy
+cp -r /usr/share/jenkins/rancher-seed/init.groovy.d /var/jenkins_home/
 
 exec /bin/tini -- /usr/share/jenkins/rancher/jenkins.sh
